91a5026fe7bc2eb21ec059326ace84e9b62e9660
Shane Lynes <shanelynes@host-studentw-139-19.dhcp.stevens-tech.edu>
2015-01-30 18:27:52 -0500

    Files for the Random Item project

---
+++ RandomItems2/RandomItems2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItems2
+//
+//  Created by Shane Lynes on 1/30/15.
+//  Copyright (c) 2015 Shane Lynes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
